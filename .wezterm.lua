30dbd9a8d29a0d9bf93b218ce94cc534de3b386f
Tae-Jong Yoo <supserver@gmail.com>
2023-12-23 14:13:11 +1100

    updated wezterm without maximise

--- .wezterm.lua
+++ .wezterm.lua
@@ -52,34 +52,36 @@ config.window_decorations = "RESIZE"
 
 config.initial_cols = 120
 config.initial_rows = 40
+config.window_frame = {
+	font = wezterm.font({ family = "Noto Sans", weight = "Regular" }),
+}
+
 config.inactive_pane_hsb = {
 	saturation = 0.6,
 	brightness = 0.6,
 }
-config.window_frame = {
-	font = wezterm.font({ family = "Noto Sans", weight = "Regular" }),
-}
-wezterm.on("gui-startup", function(cmd)
-	-- allow `wezterm start -- something` to affect what we spawn
-	-- in our initial window
-	local args = {}
-	if cmd then
-		args = cmd.args
-	end
 
-	-- Set a workspace for coding on a current project
-	-- Top pane is for the editor, bottom pane is for the build tool
-	local tab, build_pane, window = mux.spawn_window({
-		workspace = "coding",
-		args = args,
-	})
-	window:gui_window():maximize()
-	local editor_pane = build_pane:split({
-		direction = "Right",
-		size = 0.2,
-	})
-	build_pane:split({ direction = "Top" })
-	-- We want to startup in the coding workspace
-	mux.set_active_workspace("coding")
-end)
+--wezterm.on("gui-startup", function(cmd)
+--	-- allow `wezterm start -- something` to affect what we spawn
+--	-- in our initial window
+--	local args = {}
+--	if cmd then
+--		args = cmd.args
+--	end
+--
+--	-- Set a workspace for coding on a current project
+--	-- Top pane is for the editor, bottom pane is for the build tool
+--	local tab, build_pane, window = mux.spawn_window({
+--		workspace = "coding",
+--		args = args,
+--	})
+--	window:gui_window():maximize()
+--	local editor_pane = build_pane:split({
+--		direction = "Right",
+--		size = 0.2,
+--	})
+--	build_pane:split({ direction = "Top" })
+--	-- We want to startup in the coding workspace
+--	mux.set_active_workspace("coding")
+--end)
 return config
